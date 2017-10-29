94e1ac8267ee00eaae8e0c10d989a1cd2527c044
NarekAUA <narek_shamamyan@edu.aua.am>
2017-10-29 15:22:48 +0400

    first commit

---
+++ Homework5task1.js
@@ -0,0 +1,35 @@
+
+const rand = function(num) {
+	return Math.floor(Math.random() * num) + 1;
+};
+
+const colorArray = ['red', 'blue', 'green'];
+
+const createPoints = function(count, canvasWidth, canvasHeight){
+
+	const points = [];
+    
+
+	
+ 	const GcenqArrayLol = function(num){
+ 		if (num < 1) {return ""}
+ 	
+
+ 			points.push({
+ 				x: rand(canvasWidth - 30),
+ 				y: rand(canvasHeight - 30),
+ 				width: 30,
+ 				height: 30,
+ 				yDelta: 1,
+ 				xDelta: 1,
+ 				color: colorArray[rand(3)-1]
+ 			});
+ 		GcenqArrayLol(num - 1);
+ 	};
+
+ 	GcenqArrayLol(count);
+ 	return points;
+  };
+
+     
+      
